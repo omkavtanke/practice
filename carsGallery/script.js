77d3c46bb826ee384f9cc249c4f87c613a3f1781
omka <antiomka@gmail.com>
2022-12-27 10:48:23 +1100

    fix: change pop up under functional

--- carsGallery/script.js
+++ carsGallery/script.js
@@ -1,6 +1,6 @@
 //Pop up functional
 document.querySelectorAll('.cars-photos img').forEach(image =>{
-  image.onclick = () => {
+  image.addEventListener('click', () => {
     document.querySelector('.pop-up-img').src = image.src;
     document.querySelector('body').style.overflowY = 'hidden'
     document.querySelector('.pop-up-block').style.display = 'block';
@@ -9,82 +9,48 @@ document.querySelectorAll('.cars-photos img').forEach(image =>{
       document.querySelector('.chosen-mini-car').classList.remove('chosen-mini-car')
     }
     //Mini image under pop up functional
-    const CAR_NUMBER = image.id;
-    document.querySelector(`.mini-car-${CAR_NUMBER}`).classList.add('chosen-mini-car');
+    const CAR_NAME = image.getAttribute('data-car-name');
+    document.querySelector('.mini-images').innerHTML = document.querySelector(`.${CAR_NAME}-other`).innerHTML;
+    document.querySelector(`.pop-up-other .mini-car-${CAR_NAME}-1`).classList.add('chosen-mini-car');
 
     //Arrows functional
     const CURRENT_IMAGE_ID = document.querySelector('.pop-up-img').getAttribute('data-number');
     if(CURRENT_IMAGE_ID === '10'){
-      document.querySelector('.arrow-right-icon').removeEventListener('click', moveRight);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "#4d4d4d";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "auto";
-      document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
+      lastPage();
     }else if (CURRENT_IMAGE_ID === '1'){
-      document.querySelector('.arrow-left-icon').removeEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "#4d4d4d";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "auto";
-      document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
+      firstPage();
     }else{
-      document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
-      document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
+      otherPages();
     }
-    const CAR_NAME = image.getAttribute('data-car-name')
-    const CAR_IMAGES = document.querySelector(`.${CAR_NAME}-other`).innerHTML
-    document.querySelector('.mini-images').innerHTML = CAR_IMAGES;
-  };
-})
 
-//Functional of image under pop up
-document.querySelectorAll('.mini-img').forEach(miniImage => {
-  console.log(miniImage, 'upper');
-  miniImage.addEventListener('click', () => {
-    console.log(miniImage, 'under');
-    document.querySelector('.pop-up-img').src = miniImage.src;
-    const MINICAR_NUMBER = miniImage.getAttribute('data-minicar-number');
-    document.querySelector('.pop-up-img').setAttribute('data-number', MINICAR_NUMBER);
-    if(document.querySelector('.chosen-mini-car')){
-      document.querySelector('.chosen-mini-car').classList.remove('chosen-mini-car');
-    }
-    miniImage.classList.add('chosen-mini-car');
+    //Functional of image under pop up
+    document.querySelectorAll('.pop-up-other img').forEach(miniImage => {
+      miniImage.addEventListener('click', () => {
+        document.querySelector('.pop-up-img').src = miniImage.src;
+        const MINICAR_NUMBER = miniImage.getAttribute('data-minicar-number');
+        document.querySelector('.pop-up-img').setAttribute('data-number', MINICAR_NUMBER);
+        if(document.querySelector('.chosen-mini-car')) {
+          document.querySelector('.chosen-mini-car').classList.remove('chosen-mini-car');
+        }
+        miniImage.classList.add('chosen-mini-car');
 
-  //  check for zoomed image
-    if(document.querySelector('.pop-up-img').style.width === '80%'){
-      document.querySelector('.pop-up-img').style.width = '60%';
-    }
-  //  arrow functional
-    const CURRENT_IMAGE_ID = document.querySelector('.pop-up-img').getAttribute('data-number');
-    if(CURRENT_IMAGE_ID === '10'){
-      document.querySelector('.arrow-right-icon').removeEventListener('click', moveRight);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "#4d4d4d";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "auto";
-      document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
-    }else if (CURRENT_IMAGE_ID === '1'){
-      document.querySelector('.arrow-left-icon').removeEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "#4d4d4d";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "auto";
-      document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
-    }else{
-      document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
-      document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
-      document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
-      document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
-      document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
-    }
-  })
-})
+        //  check for zoomed image
+        if(document.querySelector('.pop-up-img').style.width === '80%'){
+          document.querySelector('.pop-up-img').style.width = '60%';
+        }
+        //  arrow functional
+        const CURRENT_IMAGE_ID = document.querySelector('.pop-up-img').getAttribute('data-number');
+        if(CURRENT_IMAGE_ID === '10'){
+          lastPage();
+        }else if (CURRENT_IMAGE_ID === '1'){
+          firstPage();
+        }else{
+          otherPages()
+        }
+      });
+    })
+  });
+});
 
 //Functional of "Back" button
 document.querySelector('.back-icon').addEventListener('click', () => {
@@ -142,11 +108,34 @@ function moveRight () {
   }
   document.querySelector(`.mini-car-${NEXT_IMAGE_ID}`).classList.add('chosen-mini-car');
 }
+function lastPage () {
+  document.querySelector('.arrow-right-icon').removeEventListener('click', moveRight);
+  document.querySelector('.pop-up-img-block span:last-child').style.color = "#4d4d4d";
+  document.querySelector('.pop-up-img-block span:last-child').style.cursor = "auto";
+  document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
+  document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
+  document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
+}
+function firstPage() {
+  document.querySelector('.arrow-left-icon').removeEventListener('click', moveLeft);
+  document.querySelector('.pop-up-img-block span:first-child').style.color = "#4d4d4d";
+  document.querySelector('.pop-up-img-block span:first-child').style.cursor = "auto";
+  document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
+  document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
+  document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
+}
+function otherPages() {
+  document.querySelector('.arrow-right-icon').addEventListener('click', moveRight);
+  document.querySelector('.arrow-left-icon').addEventListener('click', moveLeft);
+  document.querySelector('.pop-up-img-block span:last-child').style.color = "white";
+  document.querySelector('.pop-up-img-block span:last-child').style.cursor = "pointer";
+  document.querySelector('.pop-up-img-block span:first-child').style.color = "white";
+  document.querySelector('.pop-up-img-block span:first-child').style.cursor = "pointer";
+}
 
 // Functional for left arrow
 function moveLeft () {
   const PREV_IMAGE_ID = +document.querySelector('.pop-up-img').getAttribute('data-number') - 1;
-  console.log(PREV_IMAGE_ID)
   document.querySelector('.pop-up-img').src = document.getElementById('' + PREV_IMAGE_ID).src;
   document.querySelector('.pop-up-img').setAttribute('data-number', '' + PREV_IMAGE_ID);
   if(document.querySelector('.pop-up-img').getAttribute('data-number') === '1'){
